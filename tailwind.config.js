f08d1c346cede208da0fc627baae297807c7ab89
Navojith <navojith22@gmail.com>
2023-09-19 00:00:46 +0530

    feat: login screen ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,17 @@
 module.exports = {
   content: [
     './App.{js,jsx,ts,tsx}',
-    './components/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
     './screens/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-blue': '#0369A1',
+        'light-blue': '#0284C7',
+        'lightest-blue': '#F0F9FF',
+      },
+    },
   },
   plugins: [],
 };
