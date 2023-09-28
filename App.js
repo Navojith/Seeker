19ad0ba876ecebf279eecc39e0f65569fa09db83
Navojith <navojith22@gmail.com>
2023-09-28 22:25:35 +0530

    fix: route issue

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { auth } from './firebase';
-import UserFoundRoutes from './routes/UserFoundRoutes';
-import UserNotFoundRoutes from './routes/UserNotFoundRoutes';
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      {auth.currentUser ? (
-        <UserFoundRoutes Stack={Stack} />
-      ) : (
-        <UserNotFoundRoutes Stack={Stack} />
-      )}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="login"
+          options={{ headerShown: false }}
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          name="register"
+          options={{ headerShown: false }}
+          component={RegisterScreen}
+        />
+        <Stack.Screen name="home" component={HomeScreen} />
+        <Stack.Screen name="profile" component={ProfileScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
