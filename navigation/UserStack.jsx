02b2cd62178d1e205cdc46ba1214e5d273cfd03f
Navojith <navojith22@gmail.com>
2023-09-29 17:33:57 +0530

    feat: navbar outline

--- navigation/UserStack.jsx
+++ navigation/UserStack.jsx
@@ -12,12 +12,29 @@ const UserStack = () => {
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={{
-          tabBarStyle: { backgroundColor: '#0e1529' },
+          tabBarStyle: {
+            backgroundColor: '#0284C7',
+            height: 70,
+            paddingBottom: 10,
+            borderTopLeftRadius: 40,
+            borderTopRightRadius: 40,
+            borderTopWidth: 3,
+            borderLeftWidth: 3,
+            borderRightWidth: 3,
+            borderBottomWidth: 0,
+            borderColor: '#000',
+          },
         }}
-        sceneContainerStyle={{ backgroundColor: '#0e1529' }}
+        sceneContainerStyle={{}}
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} 
+        />
+        <Tab.Screen name="Profile" component={ProfileScreen} 
+        options={{
+            tabBarIcon: ({focused} => (
+                
+            ))
+        }}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
