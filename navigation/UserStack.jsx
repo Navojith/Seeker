b2d17222b0838490bf5a0349403c05e8f33d8250
Navojith <navojith22@gmail.com>
2023-10-28 08:55:07 +0530

    fix: active

--- navigation/UserStack.jsx
+++ navigation/UserStack.jsx
@@ -19,6 +19,8 @@ import PostedItemStack from './PostedItemStack';
 import FoundItemStack from './FoundItemStack';
 import FoundIcon from '../assets/icons/FoundIcon';
 import LostIcon from '../assets/icons/LostIcon';
+import MatIcon from 'react-native-vector-icons/MaterialIcons';
+import AntIcon from 'react-native-vector-icons/AntDesign';
 
 const Tab = createBottomTabNavigator();
 
@@ -57,16 +59,34 @@ const UserStack = () => {
           component={LeaderBoardScreen}
           options={{
             header: () => <CustomHeader title="Leaderboard" />,
-            tabBarIcon: () => <LeaderBoardIcon name="chart" size={30} />,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <MatIcon name="leaderboard" size={30} color={'white'} />
+              ) : (
+                <MatIcon name="leaderboard" size={30} color={'black'} />
+              ),
           }}
         />
         <Tab.Screen
           name={FoundItems}
           component={FoundItemStack}
           options={{
-            tabBarIcon: () => (
-              <FoundIcon name="found" size={48} style={{ marginTop: 5 }} />
-            ),
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <AntIcon
+                  name="checkcircle"
+                  size={30}
+                  style={{ marginTop: 5 }}
+                  color="white"
+                />
+              ) : (
+                <AntIcon
+                  name="checkcircle"
+                  size={30}
+                  style={{ marginTop: 5 }}
+                  color="black"
+                />
+              ),
             // headerStyle: {
             //   backgroundColor: '#0369A1',
             // },
@@ -82,9 +102,22 @@ const UserStack = () => {
           name={LostItems}
           component={LostItemStack}
           options={{
-            tabBarIcon: () => (
-              <LostIcon name="found" size={48} style={{ marginTop: 5 }} />
-            ),
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <AntIcon
+                  name="closecircle"
+                  size={30}
+                  style={{ marginTop: 5 }}
+                  color="white"
+                />
+              ) : (
+                <AntIcon
+                  name="closecircle"
+                  size={30}
+                  style={{ marginTop: 5 }}
+                  color="black"
+                />
+              ),
             // headerStyle: {
             //   backgroundColor: '#0369A1',
             // },
@@ -101,9 +134,22 @@ const UserStack = () => {
           component={PostedItemStack}
           options={{
             // header: () => <Header title="Profile" />,
-            tabBarIcon: () => (
-              <ProfileIcon name="user" size={48} style={{ marginTop: -10 }} />
-            ),
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <ProfileIcon
+                  name="user"
+                  size={48}
+                  style={{ marginTop: -10 }}
+                  color="white"
+                />
+              ) : (
+                <ProfileIcon
+                  name="user"
+                  size={48}
+                  style={{ marginTop: -10 }}
+                  color="black"
+                />
+              ),
             headerShown: false,
           }}
         />
